04a14b648f1b76bf3d5c2f6b42d89446a7a28736
nakato-proscovia <nakatoproscovia21@gmail.com>
2026-04-13 12:06:16 +0300

    modified index.html, script.js, style.css

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const input = document.getElementById("taskInput");
-const addBtn = document.getElementById("addBtn")
-const list = document.getElementById("taskList")
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("tasklist");
 
-// CRUD - Create, Read, Update, Delete
-
-// Create - create a new task 
 addBtn.addEventListener("click", function(){
     const newTask = input.value.trim();
 
@@ -14,31 +11,29 @@ addBtn.addEventListener("click", function(){
     }
 
     const li = document.createElement("li");
-    li.textContent = newTask;
-    list.appendChild(li);
+
+    const span = document.createElement("span");
+    span.textContent = newTask;
+    li.appendChild(span);
 
     const editBtn = document.createElement("button");
-    editBtn.textContent = "Edit"
+    editBtn.textContent = "Edit";
     editBtn.addEventListener("click", function(){
-        const updatedTask = prompt("Enter the updated task", li.textContent);
+        const updatedTask = prompt("Enter the updated task", span.textContent);
         if(updatedTask !== null && updatedTask.trim() !== "") {
-            li.textContent = updatedTask.trim();
-            li.appendChild(editBtn)
+            span.textContent = updatedTask.trim();
         }
-    })
-    
-    // Delete button for each task
+    });
+    li.appendChild(editBtn);
+
     const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete"
-    li.appendChild(deleteBtn);
+    deleteBtn.textContent = "Delete";
     deleteBtn.addEventListener("click", function(){
         li.remove();
-    })
+    });
+    li.appendChild(deleteBtn);
 
-
+    list.appendChild(li);
     input.value = "";
-})
+});
 
-// Read - display task (already done by default)
-
-// Edit - update a task
